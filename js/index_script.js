d23d6464cc2638e24854f96e1cec2cdec796f8a2
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2022-01-14 08:58:33 -0500

    Add files via upload

--- js/index_script.js
+++ js/index_script.js
@@ -1,87 +1,90 @@
 // Trigger function get Data on each keyup event in input
-document.getElementById('hero-name').onkeyup=getData;
+document.getElementById('hero-name').onkeyup = getData;
+
+var heroes = [];
+
+function getInitialData() {
+  fetch('https://akabab.github.io/superhero-api/api/all.json')
+    .then((res) => res.json())
+    .then((heroesResponse) => {
+      console.log('heroesResponse: ', heroesResponse);
+      heroes = heroesResponse;
+    });
+}
+
+getInitialData();
 
 //hero id
-let heroId=0;
+let heroId = 0;
 // function to get data
-function getData(){
+function getData() {
+  console.log('getData called');
+  var val = document.getElementById('hero-name').value;
+  var list = document.getElementById('auto-complete');
+  clearList();
 
-    var val = document.getElementById('hero-name').value;
-    var list = document.getElementById('auto-complete');
+  var names = heroes.filter((hero) =>
+    hero.name.toLowerCase().includes(val.toLowerCase())
+  );
+
+  if (names == null) {
     clearList();
-  
-    var xhrRequest = new XMLHttpRequest();
-    // Handling http request
-    xhrRequest.onload = function (){
-        var result=JSON.parse(xhrRequest.response);
-        // Getting all the available data
-        var names =result.results;
-
-        if(names==null){
-            clearList();
-            console.log('not found!')
-        }else{
-            for(var i of names){
-                // creating individual list item and appending it
-                var li = document.createElement('li');
-                li.innerText=i.name;
-                li.id=i.id;
-                li.classList.add('list-group-item');
-                li.addEventListener('click',function(){
-                    heroId=this.id;
-                    document.getElementById('hero-name').value=this.innerText;
-                    clearList();
-                    // brings the focus to input
-                    document.getElementById('hero-name').focus();
-                    return;
-                })
-                var ul = document.getElementById('auto-complete').appendChild(li);
-            }
-        }
-        
+    console.log('not found!');
+  } else {
+    for (var i of names) {
+      // creating individual list item and appending it
+      var li = document.createElement('li');
+      li.innerText = i.name;
+      li.id = i.id;
+      li.classList.add('list-group-item');
+      li.addEventListener('click', function () {
+        heroId = this.id;
+        document.getElementById('hero-name').value = this.innerText;
+        clearList();
+        // brings the focus to input
+        document.getElementById('hero-name').focus();
+        var urlToOpen = `superhero.html?heroId=${heroId}`;
+        var recentlyOpenedWindow = window.open(urlToOpen, '_blank');
+        recentlyOpenedWindow.sessionStorage.setItem('heroId', heroId);
+        return;
+      });
+      var ul = document.getElementById('auto-complete').appendChild(li);
     }
-
-         // xmlRequest
-        xhrRequest.open('GET','https://akabab.github.io/superhero-api/api/all.json'+val);
-        xhrRequest.send();
-        
-    }
-
+  }
+}
 // handling enter key event
 
-document.getElementById('hero-name').addEventListener('keydown',function(event){
-
-    if(event.key.code==13){
-
-        if(heroId==0){
-            alert('No hero found! Try selecting the hero from the list');
-        }else{
+document
+  .getElementById('hero-name')
+  .addEventListener('keydown', function (event) {
+    if (event.key.code == 13) {
+      if (heroId == 0) {
+        alert('No hero found! Try selecting the hero from the list');
+      } else {
         showHero();
-        }
+      }
     }
-});
+  });
 
 // Function to clear the list items from list
-function clearList(){
-
-    var list = document.getElementById('auto-complete');
-    while(list.hasChildNodes()){
-        list.removeChild(list.firstChild)
-    }
-    // heroId=null;
+function clearList() {
+  var list = document.getElementById('auto-complete');
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+  // heroId=null;
 }
 // on clicking search button
-document.getElementById('btn-search').addEventListener('click',showHero);
-function showHero(){
-    var name = document.getElementById('hero-name').value;
-    if(name==""){
-        alert("Enter the name to be searched");
-    }else if(heroId==0){
-        alert('No hero found! Try selecting the hero from the list');
-    }else{
+document.getElementById('btn-search').addEventListener('click', showHero);
 
-            window.open('superhero.html?id='+heroId,'blank');
-        
-    }
-    // console.log(hero);
+function showHero() {
+  var name = document.getElementById('hero-name').value;
+  if (name == '') {
+    alert('Enter the name to be searched');
+  } else if (heroId == 0) {
+    alert('No hero found! Try selecting the hero from the list');
+  } else {
+    window.open('superhero.html?id=' + heroId, 'blank');
+  }
 }
+
